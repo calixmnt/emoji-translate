c1d0cd0af1b70706d77766bf47bf42dc824bee5e
Calixxx <calixmonnet@gmail.com>
2024-09-11 21:59:11 +0000

    webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,59 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Pour générer automatiquement l'index.html
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), //le repertoire de destination du bundle
-    publicPath: '/'
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'src'), //le repertoire du fichier html
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.js', // Hash en production pour le cache busting
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
     },
-    compress: true,
-    port: 9000,
-    open: true,
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'src'),
+      },
+      compress: true,
+      port: 9000,
+      open: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html', // Génére automatiquement le fichier HTML en sortie
+        minify: isProduction // Minifie le fichier HTML en production
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+              removeRedundantAttributes: true,
+              useShortDoctype: true,
+            }
+          : false,
+      }),
+    ],
+    optimization: isProduction
+      ? {
+          splitChunks: {
+            chunks: 'all',
+          },
         }
-      }
-    ]
-  }
+      : {},
+  };
 };
