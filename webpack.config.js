1e16e69dcec73367c3b96d3884d8a0eefcbd5102
Calixxx <calixmonnet@gmail.com>
2024-09-17 22:51:42 +0000

    rewrite webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // Pour générer automatiquement l'index.html
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
@@ -11,13 +8,12 @@ module.exports = (env, argv) => {
     mode: isProduction ? "production" : "development",
     entry: "./src/index.js",
     output: {
-      filename: isProduction ? "bundle.[contenthash].js" : "bundle.js", // Hash en production pour le cache busting
+      filename: "bundle.js",
       path: path.resolve(__dirname, "dist"),
-      publicPath: "/",
     },
     devServer: {
       static: {
-        directory: path.resolve(__dirname, "src"),
+        directory: path.join(__dirname, "src"),
       },
       compress: true,
       port: 9000,
@@ -29,42 +25,19 @@ module.exports = (env, argv) => {
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
+          use: "babel-loader",
         },
         {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
       ],
     },
     plugins: [
-      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        template: "./src/index.html", // Génére automatiquement le fichier HTML en sortie
-        minify: isProduction // Minifie le fichier HTML en production
-          ? {
-              collapseWhitespace: true,
-              removeComments: true,
-              removeRedundantAttributes: true,
-              useShortDoctype: true,
-            }
-          : false,
-      }),
-      new MiniCssExtractPlugin({
-        filename: "style.css",
+        template: "./src/index.html",
+        minify: true,
       }),
     ],
-    optimization: isProduction
-      ? {
-          splitChunks: {
-            chunks: "all",
-          },
-        }
-      : {},
   };
 };
